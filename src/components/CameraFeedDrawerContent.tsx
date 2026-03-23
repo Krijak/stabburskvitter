2e80522104fe635c2249b2d22727d00c3d29adb5
Kristina Jakobsen <krijak@stud.ntnu.no>
2026-03-23 19:48:08 +0100

    enda lengre feilmelding

--- src/components/CameraFeedDrawerContent.tsx
+++ src/components/CameraFeedDrawerContent.tsx
@@ -1,8 +1,9 @@
-import { Collapse, Stack, Typography } from "@mui/material";
+import { Button, Collapse, Dialog, Link, Stack, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
 import blue_tit_flying from "../img/blue_tit_flying.gif";
 import type { DayPartsForecast } from "../helpers/yrWeather";
 import { getWeatherIconUrl } from "../helpers/weatherIcons";
+import { useState } from "react";
 
 export type CameraFeedDrawerContentProps = {
   showDelayedContent: boolean;
@@ -15,8 +16,10 @@ export default function CameraFeedDrawerContent({
   dayPartsWeather,
   weatherLoading,
 }: CameraFeedDrawerContentProps) {
+  const [openDialog, setOpenDialog] = useState(false);
+
   return (
-    <Stack gap={2} padding={4} alignItems="center" justifyContent="center">
+    <Stack gap={2} padding={4} paddingBottom={2} alignItems="center" justifyContent="center">
       <Stack width="6rem" justifyContent="center" alignItems="center">
         <img
           src={blue_tit_flying}
@@ -70,8 +73,30 @@ export default function CameraFeedDrawerContent({
               })}
             </Stack>
           )}
+          <Link component={Button} sx={{ fontSize: "0.75rem", textAlign: "center", mt: 3 }} onClick={() => setOpenDialog(true)}>Det er sol, hvorfor ser jeg fremdeles ikke noe?</Link>
         </Stack>
       </Collapse>
+      <Dialog open={openDialog} onClose={() => setOpenDialog(false)}>
+        <Stack p={3} gap={2} maxWidth={"30rem"}>
+          <Typography variant="body2">
+
+            Det kan det være mange grunner til! Mest sannsynlig må man bare vente til litt over kl 12, for det er først da solen skinner direkte på solcellepanelet.
+          </Typography>
+
+          <Typography variant="body2" >
+            Er klokken mer enn 12 og det har vært fint vær, så kan det hende at proxyen er nede, og hele det opplegget er litt teknisk.
+          </Typography>
+          <Typography variant="body2" >
+            Proxyen oversetter rtsp-strømmen
+            som kommer fra kameraet til et format som kan brukes til å vise videostrømmen på web. Det kan hende at proxyen har krasjet, at en ip-adresse har endret seg, at Cloudflare er nede,
+            eller at pcen som kjører proxyen ikke er koblet på nett.
+          </Typography>
+
+          <Typography textAlign={"center"}>
+            🤓
+          </Typography>
+        </Stack>
+      </Dialog>
     </Stack>
   );
 }
